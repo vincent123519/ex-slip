868892d2a56e1ba0b3f4e61dd2e377093e067e3d
CHRISTIAN R MORALES <christian.morales@usjr.edu.ph>
2024-04-02 12:09:05 +0800

    fix dean email db

--- database/migrations/2023_09_14_193254_create_deans_table.php
+++ database/migrations/2023_09_14_193254_create_deans_table.php
@@ -13,7 +13,7 @@ class CreateDeansTable extends Migration
             $table->unsignedBigInteger('user_id')->unique();
             $table->string('first_name', 100);
             $table->string('last_name', 100);
-            $table->string('email', 100)->unique(); // Add the email column
+            $table->string('email', 100)->nullable(); // Add the email column
             $table->unsignedBigInteger('school_code')->nullable()->default(1001);
             // Add foreign key constraints with the same data type
             $table->foreign('user_id')->references('user_id')->on('users');

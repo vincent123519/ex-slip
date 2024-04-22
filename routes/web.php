6a5c8369d1dfe2769120fae43177ef4d8bc4ef6c
CHRISTIAN R MORALES <christian.morales@usjr.edu.ph>
2024-04-23 00:50:36 +0800

    working studyload bulk upload

--- routes/web.php
+++ routes/web.php
@@ -158,3 +158,5 @@ Route::get('/admin/import', [AdminController::class, 'showImportForm'])->name('a
 Route::post('/admin/import-teachers', [AdminController::class, 'importTeachers'])->name('admin.import.teachers');
 Route::post('/admin/import-courses', [AdminController::class, 'importCourses'])->name('admin.import.courses');
 Route::post('/import-course-offerings', [AdminController::class, 'importCourseOfferings'])->name('import.course.offerings.form');
+Route::post('/import-study-load', [AdminController::class, 'importStudyLoad'])->name('import.studyload');
+

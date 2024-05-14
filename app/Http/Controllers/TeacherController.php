2eb2d0dce65fea409c6d1ae52a35b77e8f807672
CHRISTIAN R MORALES <christian.morales@usjr.edu.ph>
2024-05-14 23:54:30 +0800

    bug fix

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -63,6 +63,7 @@ public function dashboard()
     $sort = request('sort', 'status'); // Get the sort value from the request, default to 'status'
     $allExcuseSlips = ExcuseSlip::with('student', 'teacher', 'counselor', 'dean', 'course', 'status')
         ->select('excuse_slip_id', 'counselor_id', 'student_id', 'reason', 'dean_id', 'teacher_id', 'start_date', 'offer_code', 'end_date', 'status_id', 'read_by_teacher')
+        ->where('teacher_id', $teacherId)
         ->whereHas('status', function ($query) {
             $query->whereIn('status_name', ['Pending', 'Approved by Teacher', 'Rejected', 'Approved by Dean']);
         });
